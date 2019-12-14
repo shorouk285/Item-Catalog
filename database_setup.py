4834ffb9396c1154bd0076898364c016ad9bee1e
Shorouk AbuBatta <44466523+shorouk285@users.noreply.github.com>
2019-12-14 03:37:11 +0200

    Update database_setup.py

--- database_setup.py
+++ database_setup.py
@@ -1,3 +1,4 @@
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
